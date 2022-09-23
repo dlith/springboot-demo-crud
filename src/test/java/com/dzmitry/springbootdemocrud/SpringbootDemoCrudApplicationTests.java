a2b334d5e2c2f4fed674133c6fe88d37542f6d82
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-09-23 11:20:50 +0200

    init

---
+++ src/test/java/com/dzmitry/springbootdemocrud/SpringbootDemoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzmitry.springbootdemocrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
