c492b3c5aa9b63b55fb22259e62d3af7c2cf398a
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-09-24 17:26:21 +0200

    configuration, pagination and sorting

--- src/main/java/com/dzmitry/springbootdemocrud/dao_jpa_repository/EmployeeRepository.java
+++ src/main/java/com/dzmitry/springbootdemocrud/dao_jpa_repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.dzmitry.springbootdemocrud.dao_jpa_repository;
 
 import com.dzmitry.springbootdemocrud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
